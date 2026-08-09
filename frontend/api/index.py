00bf54120a36cef959b2ddc2e91f3303b7241c8d
Khairul Insan Al Amin <135526716+khairulInsan20@users.noreply.github.com>
2026-08-10 00:56:34 +0700

    Update index.py

--- frontend/api/index.py
+++ frontend/api/index.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-BACKEND_DIR = Path(__file__).resolve().parent.parent.parent / "backend"
+BACKEND_DIR = Path("/var/task/backend")
 sys.path.insert(0, str(BACKEND_DIR))
 
 from server import app
