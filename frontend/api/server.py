a26603794ff980e3af648eaad6e257459e4e6c46
Khairul Insan Al Amin <135526716+khairulInsan20@users.noreply.github.com>
2026-08-10 21:36:43 +0700

    Update server.py

--- frontend/api/server.py
+++ frontend/api/server.py
@@ -21,7 +21,13 @@ db = client[os.environ["DB_NAME"]]
 
 
 # Create FastAPI application
-app = FastAPI()
+app = FastAPI(
+    title="STO Desk API",
+    version="0.1.0",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json",
+)
 
 
 # Root endpoint
